2a1801dc6763659875cbaefa7548c2f78880ab2e
Melody <168307748+MelodyDevelopment@users.noreply.github.com>
2025-04-11 17:27:12 -0400

    Fix: Correct warranty wording and update license link in console log

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
+console.log(`Vorel Copyright (C) 2025 1334Robotics
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions. TO VIEW THE LICENSE VISIT OUR [GITHUB](https://github.com/1334Robotics/vorel-app/blob/main/LICENSE)
+`)
+
 const app = express();
 const Routes = require('./routes/apex');
 const apiRoutes = require('./routes/api');
